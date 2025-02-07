6db01d8ffc791c1cc334b30c2ee97d31536b0a8d
mahammad.ismayilov <m.ismayilov559@gmail.com>
2025-02-07 11:34:30 +0400

    feat: Implement navigation between login and register fragments
    - Added `LoginFragment` and `RegisterFragment` to manage the login and register screens.
    - Created `app_base_navigation.xml` to define the navigation flow between `LoginFragment` and `RegisterFragment`.
    - Set up navigation actions to switch between the two fragments using the navigation component.
    - Updated `MainActivity` to use `NavHostFragment` for managing fragment navigation.
    - Modified build.gradle to include navigation dependencies.
    - Created `fragment_login.xml` and `fragment_register.xml` to use them in the corresponding fragments.
    - Removed the login logic from `MainActivity` and added it in `LoginFragment`.
    - Removed unnecessary views and changed layout in `activity_main.xml` to include `FragmentContainerView` for navigation.
    - Added view binding to `build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
 }
 
 android {
@@ -33,9 +34,14 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+
+    val nav_version = "2.8.6"
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
@@ -45,4 +51,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.navigation:navigation-fragment:$nav_version")
+    implementation("androidx.navigation:navigation-ui:$nav_version")
 }
--- app/src/main/java/com/bootcamp/zentbc/MainActivity.kt
+++ app/src/main/java/com/bootcamp/zentbc/MainActivity.kt
@@ -1,35 +1,18 @@
 package com.bootcamp.zentbc
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.collection.emptyLongSet
+import com.bootcamp.zentbc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val btnLogin = findViewById<Button>(R.id.btn_login)
-        val txtPassword = findViewById<EditText>(R.id.edit_txt_password)
-        val txtEmail = findViewById<EditText>(R.id.edit_txt_email)
-
-        btnLogin.setOnClickListener { _ ->
-            if (txtEmail.text.isEmpty() ||
-                txtPassword.text.isEmpty()){
-                showToast("Please fill in all fields.")
-            } else if (!txtEmail.text.contains("@")){
-                showToast("Invalid email.")
-            } else if (txtPassword.length()<8){
-                showToast("Password must be at least 8 characters.")
-            } else{
-                showToast("Login successful.")
-            }
-        }
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
     fun showToast(message: String){
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
